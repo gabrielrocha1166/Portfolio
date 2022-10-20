30facd1463e63548a541eb2ae6fcdfa02a427089
Gabriel Fernandes Rocha <108475570+gabrielrocha1166@users.noreply.github.com>
2022-10-20 16:00:59 -0300

    Primeiro commit
    Meu portfólio

---
+++ script.js
@@ -0,0 +1,65 @@
+const  btn_whatsapp  =  document.querySelector ( '.whatsapp' ) ;
+const  btn_download  =  document.querySelector ( '.download' ) ;
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     var typed = new Typed(".typing", {
+         strings:["Desenvolvedor", "Freelancer"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+     var typed = new Typed(".typing-2", {
+        strings:["Desenvolvedor", "Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+});
+
+btn_whatsapp.onclick = function () {
+    open('https://api.whatsapp.com/send?phone=5575988493983&text=Ol%C3%A1!%20Acabei%20de%20ver%20seu%20portf%C3%B3lio.');
+  }
+
+btn_download.onclick = function () {
+    open('https://drive.google.com/file/d/1OD5yoabTmOOQkUfMSTWTcjLwoJ6a9V2A/view?usp=sharing');
+  }
